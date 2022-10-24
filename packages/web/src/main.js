0641d14ecd84731506df5d1f426000b2ece47e94
Vladimir Minakov <minakov@gmail.com>
2022-10-24 19:32:49 +0000

    Add pages & layouts to web

--- packages/web/src/main.js
+++ packages/web/src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { setupLayouts } from 'virtual:generated-layouts'
+import generatedRoutes from 'virtual:generated-pages'
 import './style.css'
 import App from './App.vue'
 
+const router = createRouter({
+  history: createWebHistory(),
+  routes: setupLayouts(generatedRoutes),
+})
+
 console.log(import.meta.env)
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Layouts from 'vite-plugin-vue-layouts';
+import Pages from 'vite-plugin-pages';
 
 export const envDir = process.cwd()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Pages(), Layouts()],
   envDir
 })
