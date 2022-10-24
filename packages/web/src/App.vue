8aad48b8bb382aef49a2a620f00714b334471ee6
Vladimir Minakov <minakov@gmail.com>
2022-10-24 08:35:14 +0000

    Add web package

---
+++ packages/web/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <img src="/pxflux.svg" class="logo" alt="Pxflux" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+</style>
