0588fef68c8b6815b83446e265eb04f986833c9c
Vladimir Minakov <minakov@gmail.com>
2022-10-25 06:59:00 +0000

    Add wallet connect

--- packages/web/vite.config.js
+++ packages/web/vite.config.js
@@ -1,12 +1,37 @@
 import { defineConfig } from 'vite'
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
+import nodePolyfills from 'rollup-plugin-node-polyfills'
 import vue from '@vitejs/plugin-vue'
-import Layouts from 'vite-plugin-vue-layouts';
-import Pages from 'vite-plugin-pages';
+import Layouts from 'vite-plugin-vue-layouts'
+import Pages from 'vite-plugin-pages'
 
 export const envDir = process.cwd()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), Pages(), Layouts()],
-  envDir
+  envDir,
+  resolve: {
+    alias: {
+      stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+      events: 'rollup-plugin-node-polyfills/polyfills/events',
+      crypto: 'crypto-browserify',
+      util: 'util',
+    },
+  },
+  define: {
+    global: 'globalThis',
+    'process.env': process.env ?? {},
+  },
+  build: {
+    target: 'esnext',
+    rollupOptions: {
+      plugins: [nodePolyfills({ crypto: true })],
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
+    },
+  },
 })
