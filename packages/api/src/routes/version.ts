4e51d432c3ff333d33394ec92c19dd344628068e
Vladimir Minakov <minakov@gmail.com>
2022-10-25 16:56:59 +0000

    Fix api

--- packages/api/src/routes/version.ts
+++ packages/api/src/routes/version.ts
@@ -2,10 +2,7 @@ import { getServiceConfig } from '@/config'
 import { JsonResponse } from '@/utils/json-response'
 import { TypedHandler } from '.'
 
-const { VERSION } = getServiceConfig()
-const BODY = new JsonResponse({ version: VERSION })
-
 export const version: TypedHandler = function (request, env: Bindings) {
-  console.log(env)
-  return BODY
+  const { VERSION } = getServiceConfig()
+  return new JsonResponse({ version: VERSION })
 }
