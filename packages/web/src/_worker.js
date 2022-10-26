57bb09e9e99f4b9c3c3c216a46600b3e6ea1db4c
Vladimir Minakov <minakov@gmail.com>
2022-10-26 19:10:31 +0000

    Fix worker

--- packages/web/src/_worker.js
+++ packages/web/src/_worker.js
@@ -1,9 +1,12 @@
-addEventListener('fetch', function (event) {
-  return event.respondWith(
-    new Response('<h1>hello from webworker</h1>', {
-      headers: {
-        'content-type': 'text/html',
-      },
-    })
-  )
-})
+export default {
+  async fetch(request, env) {
+    const url = new URL(request.url)
+    if (url.pathname.startsWith('/api/')) {
+      // TODO: Add your custom /api/* logic here.
+      return new Response('Ok')
+    }
+    // Otherwise, serve the static assets.
+    // Without this, the Worker will error and no assets will be served.
+    return env.ASSETS.fetch(request)
+  },
+}
