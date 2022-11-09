678e49b0a70a9a9f19e1621b4133857f67b5953e
Vladimir Minakov <minakov@gmail.com>
2022-11-09 08:00:29 +0000

    Fix worker

--- packages/web/src/public/sandbox/worker.js
+++ packages/web/src/public/sandbox/worker.js
@@ -1,6 +1,13 @@
 const cache = {}
 const referrers = {}
 
+async function fetchUrl(url, file) {
+  // Let the service worker decide on secure response headers, but set its body to the file blob.
+  const record = await fetch(url)
+  const options = /\.m?js([#?].*)?$/.test(file) ? { headers: { 'content-type': 'text/javascript' } } : undefined
+  return new Response(record.body, options)
+}
+
 self.addEventListener('fetch', (event) => {
   // get an ID from the request referrer url
   const url = new URL(event.request.referrer)
@@ -11,24 +18,34 @@ self.addEventListener('fetch', (event) => {
     // does the url matches the base ?
     if (`${url.origin}${url.pathname}` === referrers[id].base) {
       // then we prevent the default response by calling respondWith
-      event.respondWith(async function () {
-        // find the path of the resource requested
-        const path = event.request.url.replace(referrers[id].root, '')
-        // only fetch if there is a match in the cache
-        if (!cache[id][path]) { return null }
-
-        // Let the service worker decide on secure response
-        // headers, but set its body to the file blob.
-
-        const record = await fetch(cache[id][path].url)
-        return new Response(record.body)
-      }())
+      event.respondWith(
+        (async function () {
+          // find the path of the resource requested
+          const path = event.request.url.replace(referrers[id].root, '')
+          // only fetch if there is a match in the cache
+          if (!cache[id][path]) {
+            return null
+          }
+          return await fetchUrl(cache[id][path].url, event.request.url)
+        })()
+      )
+    }
+  } else {
+    // check for js modules being requested because the referrer will differ
+    const cacheId = Object.keys(cache).pop()
+    const moduleName = event.request.url.split('/').pop()
+    // only fetch when module is part of cache
+    if (cacheId && moduleName && cache[cacheId][moduleName]) {
+      event.respondWith(
+        (async function () {
+          return await fetchUrl(cache[cacheId][moduleName].url, event.request.url)
+        })()
+      )
     }
   }
 })
 
 self.addEventListener('message', async (event) => {
-  console.log(event)
   // save the sandbox url to parse requests easily
   if (event?.data?.type === 'REGISTER_REFERRER') {
     referrers[event.data.data.id] = event.data.data.referrer
@@ -41,7 +58,6 @@ self.addEventListener('message', async (event) => {
 
   if (event?.data?.type === 'GET_INDEX') {
     const id = event.data.data
-
     if (cache[id]) {
       const html = await cache[id]['index.html'].blob.text()
       event.source.postMessage({
